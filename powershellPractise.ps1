c87ec3a5510570b5fb756289ee154bf63e20949c
Admin <Admin@ADMIN>
2021-04-04 22:17:35 +0700

    Hash Table

--- powershellPractise.ps1
+++ powershellPractise.ps1
@@ -1,3 +1,44 @@
-﻿$strComputer = @("Server_1","Server_2","Server_3")
-$strComputer
+﻿PS C:\Users\Admin>
+PS C:\Users\Admin> $empNumbers= @{"Tuanngoc"=10; "Gia Minh"=20; "TruongGiang"=40}
+PS C:\Users\Admin> $empNumbers
+
+Name                           Value
+----                           -----
+Gia Minh                       20
+Tuanngoc                       10
+TruongGiang                    40
+
+PS C:\Users\Admin> $empNumbers["Tuanngoc"]
+10
+PS C:\Users\Admin> $empNumbers["Tuanngoc"]=100
+PS C:\Users\Admin> $empNumbers
+
+Name                           Value
+----                           -----
+Gia Minh                       20
+Tuanngoc                       100
+TruongGiang                    40
+
+
+PS C:\Users\Admin> $empNumbers["NewChild"]=200
+PS C:\Users\Admin> $empNumbers
+
+Name                           Value
+----                           -----
+Gia Minh                       20
+Tuanngoc                       100
+TruongGiang                    40
+NewChild                       200
+
+
+PS C:\Users\Admin> $empNumbers.remove("NewChild")
+PS C:\Users\Admin> $empNumbers
+
+Name                           Value
+----                           -----
+Gia Minh                       20
+Tuanngoc                       100
+TruongGiang                    40
+
+
 
